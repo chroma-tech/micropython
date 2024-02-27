91dba5629c620048288bb4dccb79c7832a0d1eb6
Shlomo Zippel <shlomo.zippel@gmail.com>
2024-02-26 17:25:28 -0800

    initial fern files

---
+++ ports/esp32/boards/CHROMATECH_FERN/modules/fern.py
@@ -0,0 +1,32 @@
+from micropython import const
+from machine import Pin
+
+
+LED1_DATA = const(8)
+LED1_CLOCK = const(18)
+LED2_DATA = const(17)
+LED2_CLOCK = LED1_CLOCK  # this can be GPIO0, depending on solder bridge. Default is shared with LED1_CLOCK
+LED2_CLOCK_ALT = const(0)
+
+I2S_BCK = const(47)
+I2S_MCK = const(48)
+I2S_WS = const(14)
+I2S_SDOUT = const(21)
+I2S_SDIN = const(13)
+
+SD_CMD = const(6)
+SD_CLK = const(7)
+SD_D0 = const(15)
+SD_D1 = const(16)
+SD_D2 = const(4)
+SD_D3 = const(5)
+
+NFC_SCK = const(38)
+NFC_MISO = const(39)
+NFC_MOSI = const(40)
+NFC_NSS = const(41)
+NFC_RST = const(42)
+NFC_BUSY = const(45)
+
+I2C_SDA = const(44)
+I2C_SCL = const(43)
---
+++ ports/esp32/boards/CHROMATECH_FERN/mpconfigboard.h
@@ -0,0 +1,12 @@
+#ifndef MICROPY_HW_BOARD_NAME
+// Can be set by mpconfigboard.cmake.
+#define MICROPY_HW_BOARD_NAME "Chroma.tech fern"
+#endif
+#define MICROPY_HW_MCU_NAME "ESP32S3"
+
+#define MICROPY_PY_MACHINE_DAC (0)
+
+#define MICROPY_HW_ENABLE_UART_REPL (0)
+
+#define MICROPY_HW_I2C0_SCL (43)
+#define MICROPY_HW_I2C0_SDA (44)
