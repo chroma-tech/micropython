5f8bfa3e251a426ff48d6780211ca8b26a1cfd52
Shlomo Zippel <shlomo.zippel@gmail.com>
2024-03-05 21:08:42 -0800

    pattern params are easier to use

--- ports/esp32/usermodules/modcanopy/modcanopy.c
+++ ports/esp32/usermodules/modcanopy/modcanopy.c
@@ -1,10 +1,11 @@
 #include "modcanopy.h"
 #include "py/runtime.h"
 
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(canopy_pattern_deinit_obj, canopy_pattern_deinit);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(canopy_pattern_deinit_obj,
+                                 canopy_pattern_deinit);
 
 STATIC const mp_rom_map_elem_t canopy_pattern_locals_dict_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___del__), MP_ROM_PTR(&canopy_pattern_deinit_obj) },
+    {MP_ROM_QSTR(MP_QSTR___del__), MP_ROM_PTR(&canopy_pattern_deinit_obj)},
 };
 
 STATIC MP_DEFINE_CONST_DICT(canopy_pattern_locals_dict,
@@ -12,11 +13,13 @@ STATIC MP_DEFINE_CONST_DICT(canopy_pattern_locals_dict,
 
 MP_DEFINE_CONST_OBJ_TYPE(canopy_pattern_type, MP_QSTR_Pattern,
                          MP_TYPE_FLAG_NONE, make_new, canopy_pattern_make_new,
-                         locals_dict, &canopy_pattern_locals_dict);
+                         attr, canopy_pattern_attr, locals_dict,
+                         &canopy_pattern_locals_dict);
+
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(canopy_init_obj, canopy_init);
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(canopy_clear_obj, canopy_clear);
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(canopy_render_obj, canopy_render);
-STATIC MP_DEFINE_CONST_FUN_OBJ_3(canopy_draw_obj, canopy_draw);
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(canopy_draw_obj, canopy_draw);
 
 STATIC const mp_rom_map_elem_t mp_module_canopy_globals_table[] = {
     {MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_canopy)},
