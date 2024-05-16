0790e7822f124916396ad565cc59b8e07cb2a70e
Shlomo Zippel <shlomo.zippel@gmail.com>
2024-05-16 15:20:59 -0700

    expose brightness in canopy object

--- ports/esp32/usermodules/modcanopy/modcanopy.c
+++ ports/esp32/usermodules/modcanopy/modcanopy.c
@@ -83,8 +83,13 @@ STATIC const mp_rom_map_elem_t mp_module_canopy_globals_table[] = {
 STATIC MP_DEFINE_CONST_DICT(mp_module_canopy_globals,
                             mp_module_canopy_globals_table);
 
+STATIC MP_DEFINE_CONST_OBJ_TYPE(canopy_module_type, MP_QSTR_canopy,
+                                MP_TYPE_FLAG_HAS_SPECIAL_ACCESSORS, attr,
+                                canopy_attr, locals_dict,
+                                &mp_module_canopy_globals);
+
 const mp_obj_module_t mp_module_canopy = {
-    .base = {&mp_type_module},
+    .base = {&canopy_module_type},
     .globals = (mp_obj_dict_t *)&mp_module_canopy_globals,
 };
 
